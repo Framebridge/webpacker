747a93bed631acc9a128098be0446e14dcff36a6
Russell Osborne <russell.osborne@framebridge.com>
2019-04-07 00:45:42 -0400

    Adds support for rails 4.0

--- lib/webpacker/env.rb
+++ lib/webpacker/env.rb
@@ -18,7 +18,7 @@ class Webpacker::Env
 
   private
     def current
-      Rails.env.presence_in(available_environments)
+      Rails.env if available_environments.include?(Rails.env)
     end
 
     def fallback_env_warning
